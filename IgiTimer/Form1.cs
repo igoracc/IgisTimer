ec1386d941e257c9f6466adc9e455c19ad947e2b
Igor Josić <34856681+igoracc@users.noreply.github.com>
2020-09-04 21:13:05 +0200

    Initial commit

--- IgiTimer/Form1.cs
+++ IgiTimer/Form1.cs
@@ -15,5 +15,34 @@ namespace IgiTimer
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            notifyIcon1.Visible = true;
+            notifyIcon1.BalloonTipText = textBox2.Text;
+            timer1.Interval = Convert.ToInt32(textBox1.Text) * 60000;
+            timer1.Enabled = true;
+
+            notifyIcon1.ShowBalloonTip(Convert.ToInt32(textBox1.Text) * 60000);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            notifyIcon1.ShowBalloonTip(Convert.ToInt32(textBox1.Text) * 6000);
+        }
     }
 }
